152f604e179592fd36134181953418347b6bb9d6
rooklift <16438795+rooklift@users.noreply.github.com>
2021-05-10 20:32:53 +0100

    Organise all the things

--- src/modules/hub.js
+++ src/modules/hub.js
@@ -57,58 +57,40 @@ exports.new_hub = function() {
 	return hub;
 };
 
-// ------------------------------------------------------------------------------------------------
-
 let hub_prototype = {
 
+	// Draw........................................................................................
+
 	draw: function() {
-
-		let did_draw_pv = false;
-
 		let s = this.mouse_point();
-
 		if (s) {
-			did_draw_pv = this.maindrawer.draw_pv(this.node, s);
+			if (this.maindrawer.draw_pv(this.node, s)) {				// true iff this actually happened.
+				return;
+			}
 		}
-
-		if (!did_draw_pv) {
-			this.maindrawer.draw_standard(this.node);
-		}
+		this.maindrawer.draw_standard(this.node);
 	},
 
-	mouseenter: function(s) {											// Mouse has entered some point e.g. "jj"
-		let did_draw_pv = this.maindrawer.draw_pv(this.node, s);
-		if (!did_draw_pv && this.maindrawer.last_draw_was_pv) {			// Our last draw was some other point...
-			this.maindrawer.draw_standard(this.node);
-		}
-	},
+	update_title: function() {
 
-	mouse_left_board: function() {
-		if (this.maindrawer.last_draw_was_pv) {
-			this.maindrawer.draw_standard(this.node);
-		}
-	},
-
-	try_move: function(s) {
-		let node = this.node.try_move(s);
-		this.set_node(node);
-	},
-
-	set_node: function(node, draw_graph_flag) {
-		if (!node || this.node === node) {
+		if (this.engine.problem_text()) {
+			set_title(`Ogatak: ${this.engine.problem_text()}`);
 			return;
 		}
-		this.node = node;
-		this.draw();
-		if (draw_graph_flag) {
-			this.grapher.draw_graph(this.node);
-		} else {
-			this.grapher.draw_position(this.node);
+
+		let root = this.node.get_root();
+
+		if (root.props.PB || root.props.PW) {
+			let blackname = root.get("PB") || "Unknown";
+			let whitename = root.get("PW") || "Unknown";
+			set_title(`${blackname} (B) vs ${whitename} (W)`);
+			return;
 		}
-		if (this.engine.desired) {
-			this.go();
-		}
+
+		set_title("Ogatak");
 	},
+
+	// Tabs........................................................................................
 
 	switch_tab: function(index) {
 		if (index < 0 || index >= this.tabber.tabs.length) {
@@ -153,17 +135,7 @@ let hub_prototype = {
 		this.update_title();
 	},
 
-	prev: function() {
-		if (this.node.parent) {
-			this.set_node(this.node.parent);
-		}
-	},
-
-	next: function() {
-		if (this.node.children.length > 0) {
-			this.set_node(this.node.children[0]);
-		}
-	},
+	// Files.......................................................................................
 
 	save: function(filepath) {
 		save_sgf(this.node, filepath);
@@ -255,6 +227,8 @@ let hub_prototype = {
 		}
 	},
 
+	// New game....................................................................................
+
 	new_from_config: function(force_same_tab) {
 		this.new(config.next_size, config.next_size, config.next_komi, config.next_handicap, force_same_tab);
 	},
@@ -284,6 +258,58 @@ let hub_prototype = {
 		}
 
 		this.update_title();
+	},
+
+	// Tree........................................................................................
+
+	set_node: function(node, draw_graph_flag) {
+		if (!node || this.node === node) {
+			return;
+		}
+		this.node = node;
+		this.draw();
+		if (draw_graph_flag) {
+			this.grapher.draw_graph(this.node);
+		} else {
+			this.grapher.draw_position(this.node);
+		}
+		if (this.engine.desired) {
+			this.go();
+		}
+	},
+
+	try_move: function(s) {
+		let node = this.node.try_move(s);
+		this.set_node(node);
+	},
+
+	pass: function() {
+		let node = this.node.pass();
+		this.set_node(node);
+	},
+
+	play_best: function() {
+		if (this.node.has_valid_analysis()) {
+			let s = this.node.get_board().parse_gtp_move(this.node.analysis.moveInfos[0].move);
+			if (!s) {
+				this.pass();
+			} else {
+				let node = this.node.force_move(s);
+				this.set_node(node);
+			}
+		}
+	},
+
+	prev: function() {
+		if (this.node.parent) {
+			this.set_node(this.node.parent);
+		}
+	},
+
+	next: function() {
+		if (this.node.children.length > 0) {
+			this.set_node(this.node.children[0]);
+		}
 	},
 
 	go_to_end: function() {
@@ -427,97 +453,13 @@ let hub_prototype = {
 		}
 	},
 
-	go: function() {
-		this.engine.analyse(this.node);
-	},
-
-	halt: function() {
-		this.set_autoanalysis(false);
-		this.set_autoplay(false);
-		this.engine.halt();
-	},
-
-	toggle_ponder: function() {
-		if (this.engine.desired) {
-			this.halt();
-		} else {
-			this.go();
-		}
-	},
-
-	set_autoanalysis: function(val) {
-		this.__autoanalysis = val ? true : false;
-		ipcRenderer.send("ack_autoanalysis", this.__autoanalysis);
-	},
-
-	set_autoplay: function(val) {
-		this.__autoplay = val ? true : false;
-		ipcRenderer.send("ack_autoplay", this.__autoplay);
-	},
-
-	start_autoanalysis() {
-		this.set_autoanalysis(true);
-		this.set_autoplay(false);
-		if (!this.engine.desired) {
-			this.go();
-		}
-	},
-
-	start_autoplay: function() {
-		this.set_autoanalysis(false);
-		this.set_autoplay(true);
-		if (!this.engine.desired) {
-			this.go();
-		}
-	},
-
-	play_best: function() {
-		if (this.node.has_valid_analysis()) {
-			let s = this.node.get_board().parse_gtp_move(this.node.analysis.moveInfos[0].move);
-			if (!s) {
-				this.pass();
-			} else {
-				let node = this.node.force_move(s);
-				this.set_node(node);
-			}
-		}
-	},
-
-	pass: function() {
-		let node = this.node.pass();
-		this.set_node(node);
-	},
-
-	handle_drop: function(event) {
-		if (event.dataTransfer && event.dataTransfer.files && event.dataTransfer.files[0] && event.dataTransfer.files[0].path) {
-			this.load(event.dataTransfer.files[0].path);
-		}
-	},
-
-	save_window_size: function() {
-		config.width = window.innerWidth;
-		config.height = window.innerHeight;
-		save_config();
-	},
-
-	display_props: function(rootflag) {
-		let props = rootflag ? this.node.get_root().props : this.node.props;
-		let lines = [];
-		for (let key of Object.keys(props)) {
-			lines.push(`${key}: [${props[key].join("][")}]`);
-		}
-		alert(lines.join("\n"));
-	},
-
 	forget_analysis_tree: function() {
 		this.node.forget_analysis_tree();
 		this.halt();
 		this.draw();
 	},
 
-	throw_exception: function() {
-		throw "test exception";
-	},
+	// Engine......................................................................................
 
 	receive_object: function(o) {
 
@@ -563,6 +505,65 @@ let hub_prototype = {
 		}
 	},
 
+	go: function() {
+		this.engine.analyse(this.node);
+	},
+
+	halt: function() {
+		this.set_autoanalysis(false);
+		this.set_autoplay(false);
+		this.engine.halt();
+	},
+
+	toggle_ponder: function() {
+		if (this.engine.desired) {
+			this.halt();
+		} else {
+			this.go();
+		}
+	},
+
+	set_autoanalysis: function(val) {
+		this.__autoanalysis = val ? true : false;
+		ipcRenderer.send("ack_autoanalysis", this.__autoanalysis);
+	},
+
+	set_autoplay: function(val) {
+		this.__autoplay = val ? true : false;
+		ipcRenderer.send("ack_autoplay", this.__autoplay);
+	},
+
+	start_autoanalysis() {
+		this.set_autoanalysis(true);
+		this.set_autoplay(false);
+		if (!this.engine.desired) {
+			this.go();
+		}
+	},
+
+	start_autoplay: function() {
+		this.set_autoanalysis(false);
+		this.set_autoplay(true);
+		if (!this.engine.desired) {
+			this.go();
+		}
+	},
+
+	// Misc dev stuff..............................................................................
+
+	display_props: function(rootflag) {
+		let props = rootflag ? this.node.get_root().props : this.node.props;
+		let lines = [];
+		for (let key of Object.keys(props)) {
+			lines.push(`${key}: [${props[key].join("][")}]`);
+		}
+		alert(lines.join("\n"));
+	},
+
+	throw_exception: function() {
+		throw "test exception";
+	},
+
 	reset_colours: function() {
 		for (let key of Object.keys(defaults)) {
 			if (typeof defaults[key] === "string" && defaults[key].startsWith("#")) {
@@ -573,49 +574,7 @@ let hub_prototype = {
 		this.draw();
 	},
 
-	mouse_point: function() {
-		let overlist = document.querySelectorAll(":hover");
-		for (let item of overlist) {
-			if (typeof item.className === "string") {
-				let classes = item.className.split(" ");
-				for (let c of classes) {
-					if (c.startsWith("td_")) {
-						return c.slice(3);
-					}
-				}
-			}
-		}
-		return null;
-	},
-
-	graph_click: function(event) {
-		let node = this.grapher.node_from_click(this.node, event);
-		this.set_node(node);
-	},
-
-	window_resize_checker: function() {
-		if (this.window_resize_time) {
-			this.save_window_size();
-			this.window_resize_time = null;
-		}
-		setTimeout(() => {
-			this.window_resize_checker();
-		}, 1000);
-	},
-
-	graph_draw_spinner: function() {
-		this.grapher.draw_graph(this.node);
-		setTimeout(() => {
-			this.graph_draw_spinner();
-		}, Math.max(50, config.graph_draw_delay));
-	},
-
-	active_tab_draw_spinner: function() {
-		this.tabber.draw_active_tab(this.node);
-		setTimeout(() => {
-			this.active_tab_draw_spinner();
-		}, Math.max(50, config.graph_draw_delay));
-	},
+	// Engine configuration........................................................................
 
 	set_engine: function(filepath) {
 		config.engine = filepath;
@@ -646,23 +605,75 @@ let hub_prototype = {
 		this.update_title();
 	},
 
-	update_title: function() {
+	// Spinners....................................................................................
 
-		if (this.engine.problem_text()) {
-			set_title(`Ogatak: ${this.engine.problem_text()}`);
-			return;
+	graph_draw_spinner: function() {
+		this.grapher.draw_graph(this.node);
+		setTimeout(() => {
+			this.graph_draw_spinner();
+		}, Math.max(50, config.graph_draw_delay));
+	},
+
+	active_tab_draw_spinner: function() {
+		this.tabber.draw_active_tab(this.node);
+		setTimeout(() => {
+			this.active_tab_draw_spinner();
+		}, Math.max(50, config.graph_draw_delay));
+	},
+
+	window_resize_checker: function() {
+		if (this.window_resize_time) {
+			config.width = window.innerWidth;
+			config.height = window.innerHeight;
+			save_config();
+			this.window_resize_time = null;
 		}
+		setTimeout(() => {
+			this.window_resize_checker();
+		}, 1000);
+	},
 
-		let root = this.node.get_root();
+	// Function to find where the mouse is.........................................................
 
-		if (root.props.PB || root.props.PW) {
-			let blackname = root.get("PB") || "Unknown";
-			let whitename = root.get("PW") || "Unknown";
-			set_title(`${blackname} (B) vs ${whitename} (W)`);
-			return;
+	mouse_point: function() {
+		let overlist = document.querySelectorAll(":hover");
+		for (let item of overlist) {
+			if (typeof item.className === "string") {
+				let classes = item.className.split(" ");
+				for (let c of classes) {
+					if (c.startsWith("td_")) {
+						return c.slice(3);
+					}
+				}
+			}
 		}
+		return null;
+	},
 
-		set_title("Ogatak");
+	// Events......................................................................................
+
+	mouseenter: function(s) {											// Mouse has entered some point e.g. "jj"
+		let did_draw_pv = this.maindrawer.draw_pv(this.node, s);
+		if (!did_draw_pv && this.maindrawer.last_draw_was_pv) {			// Our last draw was some other point...
+			this.maindrawer.draw_standard(this.node);
+		}
+	},
+
+	mouse_left_board: function() {
+		if (this.maindrawer.last_draw_was_pv) {
+			this.maindrawer.draw_standard(this.node);
+		}
+	},
+
+	handle_drop: function(event) {
+		if (event.dataTransfer && event.dataTransfer.files && event.dataTransfer.files[0] && event.dataTransfer.files[0].path) {
+			this.load(event.dataTransfer.files[0].path);
+		}
+	},
+
+	graph_click: function(event) {
+		let node = this.grapher.node_from_click(this.node, event);
+		this.set_node(node);
 	},
 
 };
