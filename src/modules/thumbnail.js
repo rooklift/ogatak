30cd99c7b073378fb7f6793e43b3b868a2f4d3af
rooklift <16438795+rooklift@users.noreply.github.com>
2021-05-10 23:31:18 +0100

    Update thumbnail.js

--- src/modules/thumbnail.js
+++ src/modules/thumbnail.js
@@ -2,48 +2,45 @@
 
 module.exports = function(board, square_size) {
 
-	let c = document.createElement("canvas");
-
-	if (board) {
-		c.width = square_size * board.width;
-		c.height = square_size * board.height;
-	} else {
-		c.width = square_size * 19;
-		c.height = square_size * 19;
+	if (!board) {
+		throw "thumbnail.js: board must be valid";
 	}
 
-	if (board) {
+	let c = document.createElement("canvas");
 
-		let ctx = c.getContext("2d");
+	c.width = square_size * Math.max(19, board.width);
+	c.height = square_size * Math.max(19, board.height);
 
-		ctx.fillStyle = board ? config.wood_colour : "#000000ff";
-		ctx.beginPath();
-		ctx.rect(0, 0, c.width, c.height);
-		ctx.fill();
+	let visible_x_width = square_size * board.width;
+	let visible_y_height = square_size * board.height;
 
-		for (let x = 0; x < board.width; x++) {
+	let x_offset = (c.width / 2) - (visible_x_width / 2);
+	let y_offset = (c.height / 2) - (visible_y_height / 2);
 
-			for (let y = 0; y < board.height; y++) {
+	let ctx = c.getContext("2d");
 
-				if (board.state[x][y]) {
+	ctx.fillStyle = board ? config.wood_colour : "#000000ff";
+	ctx.beginPath();
+	ctx.rect(x_offset, y_offset, visible_x_width, visible_y_height);
+	ctx.fill();
 
-					let gx = x * square_size + (square_size / 2);
-					let gy = y * square_size + (square_size / 2);
+	for (let x = 0; x < board.width; x++) {
 
-					ctx.fillStyle = board.state[x][y] === "b" ? "#000000ff" : "#ffffffff";
-					ctx.lineWidth = 0;
+		for (let y = 0; y < board.height; y++) {
 
-					ctx.beginPath();
-					ctx.arc(gx, gy, (square_size / 2 - square_size * 0.1), 0, 2 * Math.PI);
-					ctx.fill();
-				}
+			if (board.state[x][y]) {
+
+				let gx = x * square_size + (square_size / 2) + x_offset;
+				let gy = y * square_size + (square_size / 2) + y_offset;
+
+				ctx.fillStyle = board.state[x][y] === "b" ? "#000000ff" : "#ffffffff";
+				ctx.lineWidth = 0;
+
+				ctx.beginPath();
+				ctx.arc(gx, gy, (square_size / 2 - square_size * 0.1), 0, 2 * Math.PI);
+				ctx.fill();
 			}
 		}
-
-	} else {
-
-		// Uh, nothing.
-
 	}
 
 	return c.toDataURL("image/png");
