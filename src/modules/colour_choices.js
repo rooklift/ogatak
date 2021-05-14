50ca11f5242252e73bf2d2f85798c5be5cae4d79
rooklift <16438795+rooklift@users.noreply.github.com>
2021-05-14 18:04:57 +0100

    Create colour_choices.js

---
+++ src/modules/colour_choices.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const {defaults} = require("./config_io");
+
+module.exports = {
+
+	Defaults: {
+		"best_colour_black": defaults.best_colour_black,
+		"best_colour_white": defaults.best_colour_white,
+		"circle_best": defaults.circle_best
+	},
+
+	Subtle: {
+		"best_colour_black": "#ddcc99ff",
+		"best_colour_white": "#ddcc99ff",
+		"circle_best": false
+	},
+
+};
