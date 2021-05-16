861027a840d21b79852be9ab644368e4827d6a62
rooklift <16438795+rooklift@users.noreply.github.com>
2021-05-17 00:22:26 +0100

    Update colour_choices.js

--- src/modules/colour_choices.js
+++ src/modules/colour_choices.js
@@ -4,7 +4,7 @@ const {defaults} = require("./config_io");
 
 module.exports = {
 
-	Defaults: {
+	Default: {
 		"best_colour_black": defaults.best_colour_black,
 		"best_colour_white": defaults.best_colour_white,
 		"circle_best": defaults.circle_best
@@ -13,6 +13,12 @@ module.exports = {
 	Subtle: {
 		"best_colour_black": "#ddcc99ff",
 		"best_colour_white": "#ddcc99ff",
+		"circle_best": false
+	},
+
+	Ultrasubtle: {
+		"best_colour_black": defaults.wood_colour,
+		"best_colour_white": defaults.wood_colour,
 		"circle_best": false
 	},
 
