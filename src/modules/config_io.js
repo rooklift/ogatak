97c4a83a6fbbd7a8c83a1cbf068dcafe92d63d83
rooklift <16438795+rooklift@users.noreply.github.com>
2021-05-23 18:40:17 +0100

    simplify settings

--- src/modules/config_io.js
+++ src/modules/config_io.js
@@ -19,90 +19,59 @@ exports.filepath = electron.app ?
 
 exports.config = {};
 
-exports.defaults_classified = {
+exports.defaults = {
 
-	// The various classifications get used by the hub to decide what action to take if one of these is changed.
-	// Note that the actual defaults object (created below) is a flattened version of this without classifications.
+	"engine": "",
+	"engineconfig": "",
+	"weights": "",
 
-	// It is OK for something to be in multiple classications.
+	"board_line_width": 1,
+	"square_size": 34,
 
-	engine_starters: {
-		"engine": "",
-		"engineconfig": "",
-		"weights": "",
-	},
+	"info_font_size": 16,
 
-	board_rebuilders: {
-		"board_line_width": 1,
-		"square_size": 34,
-	},
+	"thumbnail_square_size": 4,
 
-	infodiv_rebuilders: {
-		"info_font_size": 16,
-	},
+	"board_font_size": 14,
+	"best_colour_black": "#99dd55ff",
+	"best_colour_white": "#99dd55ff",
+	"wood_colour": "#d2b074ff",
+	"previous_marker": "#888888ff",
+	"candidate_moves": true,
+	"numbers": "lcb",
+	"visits_threshold": 0.02,
+	"mouseover_pv": true,
+	"next_move_markers": true,
+	"circle_best": true,
 
-	tab_rebuilders: {
-		"thumbnail_square_size": 4,
-	},
+	"graph_type": "winrate",
+	"minor_graph_linewidth": 1,
+	"major_graph_linewidth": 2,
+	"minor_graph_colour": "#444444ff",
+	"major_graph_colour": "#4ba28bff",
+	"major_graph_var_colour": "#4b8ba2ff",
+	"midline_graph_colour": "#222222ff",
+	"graph_width": 224,
 
-	board_redrawers: {
-		"board_font_size": 14,
-		"best_colour_black": "#99dd55ff",
-		"best_colour_white": "#99dd55ff",
-		"wood_colour": "#d2b074ff",
-		"previous_marker": "#888888ff",
-		"candidate_moves": true,
-		"numbers": "lcb",
-		"visits_threshold": 0.02,
-		"mouseover_pv": true,
-		"next_move_markers": true,
-		"circle_best": true,
-		"dead_stone_prediction": true,			// Duplicate
-	},
+	"tree_spacing": 24,
+	"tree_node_colour": "#909090ff",
+	"central_node_colour": "#d2b074ff",
 
-	graph_redrawers: {
-		"graph_type": "winrate",
-		"minor_graph_linewidth": 1,
-		"major_graph_linewidth": 2,
-		"minor_graph_colour": "#444444ff",
-		"major_graph_colour": "#4ba28bff",
-		"major_graph_var_colour": "#4b8ba2ff",
-		"midline_graph_colour": "#222222ff",
-		"graph_width": 224,
-	},
+	"widerootnoise": true,
 
-	tree_redrawers: {
-		"tree_spacing": 24,
-		"tree_node_colour": "#909090ff",
-		"central_node_colour": "#d2b074ff",
-	},
+	"dead_stone_prediction": true,
 
-	search_changers: {
-		"widerootnoise": true,
-		"dead_stone_prediction": true,			// Duplicate
-	},
-
-	no_immediate_effect: {
-		"width": 1200,
-		"height": 740,
-		"auto_square_size": true,
-		"stderr_to_console": true,
-		"load_at_end": true,
-		"tree_draw_delay": 17,					// Note that the tree draw will be skipped if not needed.
-		"graph_draw_delay": 200,				// The graph draw, however, is never skipped.
-		"autoanalysis_visits": 500,
-		"default_rules": "Chinese",				// Used for game on startup, as well as when rules are "Unknown".
-		"default_komi": 7.5,					// Used for game on startup, but otherwise unknown komi is inferred as zero.
-	},
+	"width": 1200,
+	"height": 740,
+	"auto_square_size": true,
+	"stderr_to_console": true,
+	"load_at_end": true,
+	"tree_draw_delay": 17,					// Note that the tree draw will be skipped if not needed.
+	"graph_draw_delay": 200,				// The graph draw, however, is never skipped.
+	"autoanalysis_visits": 500,
+	"default_rules": "Chinese",				// Used for game on startup, as well as when rules are "Unknown".
+	"default_komi": 7.5,					// Used for game on startup, but otherwise unknown komi is inferred as zero.
 };
-
-exports.defaults = {};
-
-for (let cl of Object.keys(exports.defaults_classified)) {
-	for (let key of Object.keys(exports.defaults_classified[cl])) {
-		exports.defaults[key] = exports.defaults_classified[cl][key];
-	}
-}
 
 exports.colour_keys = Object.keys(exports.defaults).filter(key => typeof exports.defaults[key] === "string" && exports.defaults[key][0] === "#");
 
