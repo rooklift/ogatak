51442fddbc558bf629ff4b8299bb69b7bb6f6b62
rooklift <16438795+rooklift@users.noreply.github.com>
2021-06-21 23:15:04 +0100

    Update colour_choices.js

--- src/modules/colour_choices.js
+++ src/modules/colour_choices.js
@@ -4,14 +4,14 @@ const {defaults} = require("./config_io");
 
 module.exports = {
 
-	Green: {
+	"Blue / Green": {
 		"top_colour_black": "#77ddddff",
 		"top_colour_white": "#77ddddff",
 		"off_colour_black": "#99dd55ff",
 		"off_colour_white": "#99dd55ff",
 	},
 
-	Transparent: {
+	"Transparent": {
 		"top_colour_black": defaults.wood_colour,
 		"top_colour_white": defaults.wood_colour,
 		"off_colour_black": defaults.wood_colour,
