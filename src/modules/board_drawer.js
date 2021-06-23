1b9cb70d25c01be5a402f9d790c55603762ebec5
rooklift <16438795+rooklift@users.noreply.github.com>
2021-06-23 19:26:17 +0100

    1 px adjustment to text_two

--- src/modules/board_drawer.js
+++ src/modules/board_drawer.js
@@ -84,7 +84,7 @@ let board_drawer_prototype = {
 		ctx.font = `${config.board_font_size}px Arial`;
 		ctx.fillStyle = colour;
 		let gx = x * config.square_size + (config.square_size / 2);
-		let gy = y * config.square_size + (config.square_size / 3);
+		let gy = y * config.square_size + (config.square_size / 3) - 1;
 		ctx.fillText(msg, gx, gy + 1);
 		gy = y * config.square_size + (config.square_size * 0.6667);
 		ctx.fillText(msg2, gx, gy + 1);
