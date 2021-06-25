d283608161dc79cc11e5f14f505469376ada9846
rooklift <16438795+rooklift@users.noreply.github.com>
2021-06-25 12:33:19 +0100

    next_marker_linewidth = 2.8

--- src/modules/config_io.js
+++ src/modules/config_io.js
@@ -41,7 +41,7 @@ exports.defaults = {
 	"off_colour_white": "#99dd55ff",
 	"wood_colour": "#d2b074ff",
 	"previous_marker": "#888888ff",
-	"next_marker_linewidth": 2.5,
+	"next_marker_linewidth": 2.8,
 	"candidate_moves": true,
 	"numbers": "lcb + visits",
 	"visits_threshold": 0.02,
