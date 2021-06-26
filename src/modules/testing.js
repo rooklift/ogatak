c2fde716c5277dd1a258d2b991068fb24c38c104
rooklift <16438795+rooklift@users.noreply.github.com>
2021-06-26 18:39:52 +0100

    use the laggy position

--- src/modules/testing.js
+++ src/modules/testing.js
@@ -1,15 +1,9 @@
 "use strict";
 
-let testpos = `(;SZ[19]KM[5.5]RE[W+R]RU[Japanese];B[pd];W[dc];B[pp];W[ep];B[cf];W[cd];B[ci];W[nc];B[qf];W[pb]
-(;B[cq];W[do];B[er];W[cp];B[dq];W[ck];B[qc];W[kc];B[gq];W[ek](;B[nq];W[go];B[ef];W[gd];B[ei];W[pk];B[ip];W[in];B[ko];W[qn]
-(;B[qi];W[mp];B[mn];W[np];B[mq];W[kp];B[lp];W[oq];B[or];W[op](;B[pq];W[pr];B[nr];W[po];B[qr];W[lo];B[lq];W[ln];B[mo];W[on]
-(;B[lm];W[kn];B[km];W[jn];B[nl];W[oj];B[lj];W[ik];B[rp];W[rj](;B[ri];W[ni];B[oo];W[no];B[nn];W[oo];B[om];W[pm];B[qo];W[pn]
-(;B[rn];W[pl];B[mj];W[lh];B[jj];W[ne];B[ij];W[hk];B[jd];W[jc](;B[id];W[jh];B[ic];W[jk];B[hi];W[hg];B[ig];W[ih];B[hh];W[if]
-(;B[mh];W[mg];B[gg];W[jg];B[mi];W[nh];B[lg];W[mf];B[kd];W[gf](;B[lc];W[kj];B[ki];W[kk];B[kh];W[kg];B[ji];W[lb];B[gc];W[gh]
-(;B[gi];W[fg];B[ej];W[fc];B[hb];W[gb];B[hd];W[hc];B[og];W[ng](;B[gc];W[fb];B[md];W[nd];B[jb];W[kb];B[le];W[lf];B[mb];W[ja]
-(;B[cn];W[mm];B[nm];W[ml];B[ib];W[mc];B[la];W[ld];B[ll];W[mk](;B[lc];W[bi];B[ge];W[ch];B[dh];W[cj];B[ka];W[di];B[fk];W[el]
-(;B[eg];W[fh];B[eh];W[fd];B[fl];W[em];B[fi];W[hj];B[lk];W[ii])(;B[fe]))(;B[lk]))(;B[fk]))(;B[fk]))(;B[nj]))(;B[fg]))
-(;B[jg]))(;B[kh]))(;B[pl]))(;B[kd]))(;B[kn]))(;B[pr]))(;B[pn]))(;B[qn]))(;B[cp]))
+let testpos = `(;SZ[19]KM[5.5]RE[W+R]RU[Japanese]
+;B[qd];W[dc];B[cp];W[pp];B[nq];W[ep];B[gq];W[fo];B[cm];W[hp];B[qn];W[pn];B[pm];W[on]
+;B[qo];W[qp];B[kp];W[pk];B[po];W[oo];B[op];W[om];B[qm];W[pl];B[rp];W[rq];B[qq];W[pq]
+;B[qr];W[ro];B[sp];W[sq];B[pr];W[so];B[rp];W[sp];B[oq];W[rp];B[rl];W[rk];B[rr])
 `;
 
 exports.load = function() {
