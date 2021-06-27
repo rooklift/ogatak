a59db63dd93d2cb24fbb436372c9b25cc2d26275
rooklift <16438795+rooklift@users.noreply.github.com>
2021-06-27 16:45:15 +0100

    Green / Yellow

--- src/modules/colour_choices.js
+++ src/modules/colour_choices.js
@@ -11,6 +11,13 @@ module.exports = {
 		"off_colour_white": "#99dd55ff",
 	},
 
+	"Green / Yellow": {
+		"top_colour_black": "#99dd55ff",
+		"top_colour_white": "#99dd55ff",
+		"off_colour_black": "#dddd55ff",
+		"off_colour_white": "#dddd55ff",
+	},
+
 	"Transparent": {
 		"top_colour_black": defaults.wood_colour,
 		"top_colour_white": defaults.wood_colour,
