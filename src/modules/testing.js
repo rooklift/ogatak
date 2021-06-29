e0e5699157f9104d7e1a524a4e44f4b42189a271
rooklift <16438795+rooklift@users.noreply.github.com>
2021-06-29 16:02:35 +0100

    speedtest()

--- src/modules/testing.js
+++ src/modules/testing.js
@@ -1,10 +1,15 @@
 "use strict";
 
-let testpos = `(;SZ[19]KM[5.5]RE[W+R]RU[Japanese]
-;B[qd];W[dc];B[cp];W[pp];B[nq];W[ep];B[gq];W[fo];B[cm];W[hp];B[qn];W[pn];B[pm];W[on]
-;B[qo];W[qp];B[kp];W[pk];B[po];W[oo];B[op];W[om];B[qm];W[pl];B[rp];W[rq];B[qq];W[pq]
-;B[qr];W[ro];B[sp];W[sq];B[pr];W[so];B[rp];W[sp];B[oq];W[rp];B[rl];W[rk];B[rr])
-`;
+const path = require("path");
+
+let testpos = `(;SZ[19]KM[7.5]RE[W+R]RU[Chinese]
+;B[pd];W[dp];B[cd];W[qp];B[op];W[oq];B[nq];W[pq];B[cn];W[fq];B[mp];W[po]
+;B[iq];W[ec];B[hd];W[cg];B[ed];W[cj];B[dc];W[bp];B[nc];W[qi];B[ep];W[eo]
+;B[dk];W[fp];B[ck];W[dj];B[ej];W[ei];B[fi];W[eh];B[fh];W[bj];B[fk];W[fg]
+;B[gg];W[ff];B[gf];W[mc];B[md];W[lc];B[nb];W[id];B[hc];W[jg];B[pj];W[pi]
+;B[oj];W[oi];B[ni];W[nh];B[mh];W[ng];B[mg];W[mi];B[nj];W[mf];B[li];W[ne]
+;B[nd];W[mj];B[lf];W[mk];B[me];W[nf];B[lh];W[qj];B[kk];W[ik];B[ji];W[gh]
+;B[hj];W[ge];B[he];W[fd];B[fc])`;
 
 exports.load = function() {
 	hub.load_sgf_from_string(testpos);
@@ -53,3 +58,26 @@ exports.stress = function(moves, cycles, delay) {
 	hub.go();
 	stresser(0, moves, cycles, delay, false, [], performance.now());
 };
+
+exports.speedtest = function(visits) {
+
+	hub.halt();
+	hub.clear_cache();
+	hub.forget_analysis_tree();
+	hub.go_to_end();
+	hub.go();
+
+	let starttime = performance.now();
+
+	let checker = () => {
+		if (hub.node && hub.node.has_valid_analysis() && hub.node.analysis.rootInfo.visits > visits) {
+			console.log(`${path.basename(config.engine)}: ${visits} visits took ${(performance.now() - starttime).toFixed(0)} ms.`);
+			hub.halt();
+		} else {
+			setTimeout(checker, 100);
+		}
+	};
+
+	checker();
+};
+
