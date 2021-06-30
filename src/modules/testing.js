2d85757ee2189db0699e7921b0b079c24342b46c
rooklift <16438795+rooklift@users.noreply.github.com>
2021-06-30 01:05:56 +0100

    positions

--- src/modules/testing.js
+++ src/modules/testing.js
@@ -2,17 +2,25 @@
 
 const path = require("path");
 
-let testpos = `(;SZ[19]KM[7.5]RE[W+R]RU[Chinese]
-;B[pd];W[dp];B[cd];W[qp];B[op];W[oq];B[nq];W[pq];B[cn];W[fq];B[mp];W[po]
-;B[iq];W[ec];B[hd];W[cg];B[ed];W[cj];B[dc];W[bp];B[nc];W[qi];B[ep];W[eo]
-;B[dk];W[fp];B[ck];W[dj];B[ej];W[ei];B[fi];W[eh];B[fh];W[bj];B[fk];W[fg]
-;B[gg];W[ff];B[gf];W[mc];B[md];W[lc];B[nb];W[id];B[hc];W[jg];B[pj];W[pi]
-;B[oj];W[oi];B[ni];W[nh];B[mh];W[ng];B[mg];W[mi];B[nj];W[mf];B[li];W[ne]
-;B[nd];W[mj];B[lf];W[mk];B[me];W[nf];B[lh];W[qj];B[kk];W[ik];B[ji];W[gh]
-;B[hj];W[ge];B[he];W[fd];B[fc])`;
+let positions = [
+	`(;SZ[19]KM[5.5]RE[W+R]RU[Japanese]
+	;B[qd];W[dc];B[cp];W[pp];B[nq];W[ep];B[gq];W[fo];B[cm];W[hp];B[qn];W[pn]
+	;B[pm];W[on];B[qo];W[qp];B[kp];W[pk];B[po];W[oo];B[op];W[om];B[qm];W[pl]
+	;B[rp];W[rq];B[qq];W[pq];B[qr];W[ro];B[sp];W[sq];B[pr];W[so];B[rp];W[sp]
+	;B[oq];W[rp];B[rl];W[rk];B[rr])`
+,
+	`(;SZ[19]KM[7.5]RE[W+R]RU[Chinese]
+	;B[pd];W[dp];B[cd];W[qp];B[op];W[oq];B[nq];W[pq];B[cn];W[fq];B[mp];W[po]
+	;B[iq];W[ec];B[hd];W[cg];B[ed];W[cj];B[dc];W[bp];B[nc];W[qi];B[ep];W[eo]
+	;B[dk];W[fp];B[ck];W[dj];B[ej];W[ei];B[fi];W[eh];B[fh];W[bj];B[fk];W[fg]
+	;B[gg];W[ff];B[gf];W[mc];B[md];W[lc];B[nb];W[id];B[hc];W[jg];B[pj];W[pi]
+	;B[oj];W[oi];B[ni];W[nh];B[mh];W[ng];B[mg];W[mi];B[nj];W[mf];B[li];W[ne]
+	;B[nd];W[mj];B[lf];W[mk];B[me];W[nf];B[lh];W[qj];B[kk];W[ik];B[ji];W[gh]
+	;B[hj];W[ge];B[he];W[fd];B[fc])`
+];
 
-exports.load = function() {
-	hub.load_sgf_from_string(testpos);
+exports.load = function(n = 0) {
+	hub.load_sgf_from_string(positions[n]);
 };
 
 // ------------------------------------------------------------------------------------------------
