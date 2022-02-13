047fe52ab94bd2f7ad7c289c778155b94a73176a
rooklift <16438795+rooklift@users.noreply.github.com>
2022-02-13 16:01:05 +0000

    display: none until script starts

--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,8 @@
 "use strict";
 
+// The gridder has display: none to start with, so that it's not drawn until the script is active.
+// Probably best to do this at the very start, so calculated styles etc work...
+
+gridder.style["display"] = "grid";
+
 require("./modules/__start");
