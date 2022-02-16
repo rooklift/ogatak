12766653ea93b59f2aa89f8ba673fa8a4ebcac0c
rooklift <16438795+rooklift@users.noreply.github.com>
2022-02-16 15:12:54 +0000

    Update renderer.js

--- src/renderer.js
+++ src/renderer.js
@@ -3,6 +3,6 @@
 // The gridder has display: none to start with, so that it's not drawn until the script is active.
 // Probably best to do this at the very start, so calculated styles etc work...
 
-gridder.style["display"] = "grid";
+document.getElementById("gridder").style["display"] = "grid";
 
 require("./modules/__start");
