29cebb48b45830e4ec71d69b809572b894653103
rooklift <16438795+rooklift@users.noreply.github.com>
2022-03-03 20:47:21 +0000

    Add some colours

--- src/modules/colour_choices.js
+++ src/modules/colour_choices.js
@@ -4,20 +4,57 @@ const {defaults} = require("./config_io");
 
 // Note that the alpha values are ignored (true on 2022-03-02).
 
+const red = "#ff7777ff";
+const blue = "#77ddddff";
+const green = "#99dd55ff";
+const yellow = "#dddd55ff";
+
+const blue_bright = "#77eeffff";
+const green_bright = "#99ff55ff";
+const yellow_bright = "#ffff55ff";
+
 module.exports = {
 
 	"Blue / Green": {
-		"top_colour_black": "#77ddddff",
-		"top_colour_white": "#77ddddff",
-		"off_colour_black": "#99dd55ff",
-		"off_colour_white": "#99dd55ff",
+		"top_colour_black": blue,
+		"top_colour_white": blue,
+		"off_colour_black": green,
+		"off_colour_white": green,
+	},
+
+	"Blue / Green (brighter)": {
+		"top_colour_black": blue_bright,
+		"top_colour_white": blue_bright,
+		"off_colour_black": green_bright,
+		"off_colour_white": green_bright,
 	},
 
 	"Green / Yellow": {
-		"top_colour_black": "#99dd55ff",
-		"top_colour_white": "#99dd55ff",
-		"off_colour_black": "#dddd55ff",
-		"off_colour_white": "#dddd55ff",
+		"top_colour_black": green,
+		"top_colour_white": green,
+		"off_colour_black": yellow,
+		"off_colour_white": yellow,
+	},
+
+	"Green / Yellow (brighter)": {
+		"top_colour_black": green_bright,
+		"top_colour_white": green_bright,
+		"off_colour_black": yellow_bright,
+		"off_colour_white": yellow_bright,
+	},
+
+	"Red": {
+		"top_colour_black": red,
+		"top_colour_white": red,
+		"off_colour_black": red,
+		"off_colour_white": red,
+	},
+
+	"Green": {
+		"top_colour_black": green,
+		"top_colour_white": green,
+		"off_colour_black": green,
+		"off_colour_white": green,
 	},
 
 };
