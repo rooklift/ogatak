2d6395ce2bfa53b76b8302a18ba492f4eaf40f14
rooklift <16438795+rooklift@users.noreply.github.com>
2022-03-06 16:24:11 +0000

    remove force_set()

--- src/modules/node.js
+++ src/modules/node.js
@@ -49,13 +49,6 @@ function new_node(parent) {
 let node_prototype = {
 
 	set: function(key, value) {
-		if (this.__board) {
-			throw "set() called on node but board already existed";
-		}
-		this.props[key] = [stringify(value)];
-	},
-
-	force_set: function(key, value) {
 		this.props[key] = [stringify(value)];
 	},
 
@@ -547,13 +540,13 @@ let node_prototype = {
 
 	coerce_komi: function(value) {
 		let root = this.get_root();
-		root.force_set("KM", value);
+		root.set("KM", value);
 		coerce_board_prop_recursive(root, "komi", value);
 	},
 
 	coerce_rules: function(value) {
 		let root = this.get_root();
-		root.force_set("RU", value);
+		root.set("RU", value);
 		coerce_board_prop_recursive(root, "rules", value);
 	},
 
