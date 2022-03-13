e76da7afa0c0ec6db00e73dd9b41dae8fde25558
rooklift <16438795+rooklift@users.noreply.github.com>
2022-03-13 17:41:44 +0000

    store pending_mousemove_y in grapher

--- src/modules/__start_spinners.js
+++ src/modules/__start_spinners.js
@@ -4,26 +4,33 @@ const {node_id_from_search_id, get_href_query_val} = require("./utils");
 
 const zoomfactor = parseFloat(get_href_query_val("zoomfactor"));
 
+// ------------------------------------------------------------------------------------------------
 
 (function active_tab_draw_spinner() {
 	tabber.draw_active_tab(hub.node);
 	setTimeout(active_tab_draw_spinner, 200);
 })();
 
+// ------------------------------------------------------------------------------------------------
+
 (function graph_draw_spinner() {
 	grapher.draw_graph(hub.node, true);			// Always does a full draw, seems fast enough.
 	setTimeout(graph_draw_spinner, 200);
 })();
+
+// ------------------------------------------------------------------------------------------------
 
 (function tree_draw_spinner() {
 	tree_drawer.draw_tree(hub.node);			// Can skip the draw if not needed.
 	setTimeout(tree_draw_spinner, 17);
 })();
 
-(function up_down_spinner() {
+// ------------------------------------------------------------------------------------------------
+// The point of this is to avoid a situation where a draw is slow, while up/down events are piling
+// up, leading to a bunch of draws being required all at once. Instead of that, up/down events just
+// adjust a variable, and this spinner does the actual draws.
 
-	// The point of this is to avoid a situation where a draw is slow, while up/down events are piling up, leading to a bunch of draws
-	// being required all at once. Instead of that, up/down events just adjust a variable, and this spinner does the actual draws.
+(function up_down_spinner() {
 
 	let n = hub.pending_up_down;
 	hub.pending_up_down = 0;
@@ -41,10 +48,12 @@ const zoomfactor = parseFloat(get_href_query_val("zoomfactor"));
 	setTimeout(up_down_spinner, 10);
 })();
 
-(function window_resize_spinner() {
+// ------------------------------------------------------------------------------------------------
+// Poll the window size; adjust our settings if needed. Does nothing if main.js has told us we are
+// maxed (by setting config.maxed). There is a race condition here -- the spinner might run after
+// the maximize but before hub has told us about it -- but meh.
 
-	// Poll the window size; adjust our settings if needed. Does nothing if main.js has told us we are maxed (by setting config.maxed).
-	// There is a race condition here -- the spinner might run after the maximize but before hub has told us about it -- but meh.
+(function window_resize_spinner() {
 
 	if (!config.maxed) {
 
@@ -62,12 +71,12 @@ const zoomfactor = parseFloat(get_href_query_val("zoomfactor"));
 	setTimeout(window_resize_spinner, 125);
 })();
 
+// ------------------------------------------------------------------------------------------------
+// Our board drawer can draw ownership marks based on ancestor nodes, on the assumption that real
+// data for the current node will soon arrive. But if the search ends and it never does we need
+// to clear those tentative ownership marks.
+
 (function bad_ownership_spinner() {
-
-	// Our board drawer can draw ownership marks based on ancestor nodes, on the assumption that real
-	// data for the current node will soon arrive. But if the search ends and it never does we need
-	// to clear those tentative ownership marks.
-
 	if (board_drawer.has_drawn_ownership) {
 		if (!hub.node.has_valid_analysis() || !hub.node.analysis.ownership) {
 			if (!hub.engine.desired || node_id_from_search_id(hub.engine.desired.id) !== hub.node.id) {
@@ -80,24 +89,23 @@ const zoomfactor = parseFloat(get_href_query_val("zoomfactor"));
 	setTimeout(bad_ownership_spinner, 190);
 })();
 
+// ------------------------------------------------------------------------------------------------
 // This next bit requires both a spinner and an event handler. It manages dragging on the grapher,
 // while limiting updates to 25 per second...
 
-let pending_grapher_mousemove_y = null;
-
 grapher.positioncanvas.addEventListener("mousemove", (event) => {
 	if (event.buttons) {
-		pending_grapher_mousemove_y = event.offsetY;
+		grapher.pending_mousemove_y = event.offsetY;
 	}
 });
 
 (function graph_mousemove_spinner() {
-	if (pending_grapher_mousemove_y !== null) {
-		let node = grapher.node_from_click_y(hub.node, pending_grapher_mousemove_y);
+	if (typeof grapher.pending_mousemove_y === "number") {
+		let node = grapher.node_from_click_y(hub.node, grapher.pending_mousemove_y);
 		if (node) {
 			hub.set_node(node, {bless: false});
 		}
-		pending_grapher_mousemove_y = null;
 	}
+	grapher.pending_mousemove_y = null;
 	setTimeout(graph_mousemove_spinner, 40);
 })();
