f30a0b7f5abb6745815ff5d4cfcb6bc9375d90b5
rooklift <16438795+rooklift@users.noreply.github.com>
2022-04-11 15:46:26 +0100

    Update hub.js

--- src/modules/hub.js
+++ src/modules/hub.js
@@ -389,7 +389,7 @@ let hub_main_props = {
 			grapher.draw_position(this.node);
 		}
 
-		tree_drawer.must_draw = true;
+		tree_drawer.must_draw = true;			// For reasons, this actually is responsible for closing special panes (fullbox etc).
 		comment_drawer.draw(this.node);
 		comment_drawer.textarea.blur();
 
