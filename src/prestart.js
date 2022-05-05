1d0adb55a5156f1a4510aba4dbfde91bd084b51b
rooklift <16438795+rooklift@users.noreply.github.com>
2022-05-05 22:07:57 +0100

    Update prestart.js

--- src/prestart.js
+++ src/prestart.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// This is not an Electron preload script. Rather, it's the main entry for the main process,
+// and allows the app to quickly check requestSingleInstanceLock() and quit if needed...
+
 if (!process || !process.versions || Number.isNaN(parseInt(process.versions.electron, 10)) || parseInt(process.versions.electron, 10) < 6) {
 	throw new Error("Ogatak requires Electron 6 or higher.");		// Because we use some promise-based APIs that started in 6.0.0
 }
