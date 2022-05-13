79d742099df025ff36bb73a51fc4e5e66539a95e
rooklift <16438795+rooklift@users.noreply.github.com>
2022-05-13 11:49:20 +0100

    no need to always set SZ and KM if not present

--- src/modules/load_gib.js
+++ src/modules/load_gib.js
@@ -14,9 +14,7 @@ function load_gib(buf) {
 	root.set("GM", 1);
 	root.set("FF", 4);
 	root.set("CA", "UTF-8");
-	root.set("SZ", 19);								// Is this always so?
 	root.set("RU", "Korean");
-	root.set("KM", 0);								// Can get adjusted in a moment.
 
 	for (let line of lines) {
 
