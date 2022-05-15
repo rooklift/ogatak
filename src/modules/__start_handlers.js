99f4d79c7516bcd20a0fb92df8952b25f8bab4c2
rooklift <16438795+rooklift@users.noreply.github.com>
2022-05-15 18:47:54 +0100

    Update __start_handlers.js

--- src/modules/__start_handlers.js
+++ src/modules/__start_handlers.js
@@ -63,7 +63,7 @@ board_drawer.htmltable.addEventListener("mousedown", (event) => {
 
 // Clicking on the boardinfo...
 
-board_drawer.info1span.addEventListener("mousedown", (event) => {
+board_drawer.infodiv.addEventListener("mousedown", (event) => {
 	event.preventDefault();
 	let s = event_path_class_string(event, "boardinfo_");
 	if (s === "rules") {
@@ -74,13 +74,7 @@ board_drawer.info1span.addEventListener("mousedown", (event) => {
 		hub.cycle_numbers(event.which !== 1);
 	} else if (s === "mode") {
 		hub.cycle_mode(event.which !== 1);
-	}
-});
-
-board_drawer.info2span.addEventListener("mousedown", (event) => {
-	event.preventDefault();
-	let s = event_path_class_string(event, "boardinfo_");
-	if (s === "active") {
+	} else if (s === "active") {
 		hub.toggle_active_player();
 	}
 });
