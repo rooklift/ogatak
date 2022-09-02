7ad057002abdf2ed8d93b555d7ff37ec7708e4c9
rooklift <16438795+rooklift@users.noreply.github.com>
2022-09-03 00:22:26 +0100

    Create engine_gtp.js

---
+++ src/modules/engine_gtp.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// My initial plan is to have our GTP engine object have the same API
+// as the normal one, including storing .running and .desired queries
+// in the normal object format.
+//
+// When it comes to actually sending and receiving, we will convert
+// to and from the object / string formats.
+
+function new_engine() {
+	// TODO
+}
+
+
+
+module.exports = new_engine;
