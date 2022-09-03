0c757f998defec8c6ea0fcf7e96b75b17922d2e6
rooklift <16438795+rooklift@users.noreply.github.com>
2022-09-03 16:02:06 +0100

    Update engine_gtp.js

--- src/modules/engine_gtp.js
+++ src/modules/engine_gtp.js
@@ -4,12 +4,14 @@
 // as the normal one, including storing .running and .desired queries
 // in the normal object format.
 //
-// When it comes to actually sending and receiving, we will convert
-// to and from the object / string formats.
+// When it comes to actually sending and receiving, we will convert to
+// and from the object / string formats.
 //
 // We should also:
 // - Store what commands are available
 // - Store the current boardsize and komi
+//
+// We also cannot record all stderr, since Leela sends a lot of it.
 
 const child_process = require("child_process");
 const fs = require("fs");
