13a2068ef133ab668aa273910679d182445fe608
rooklift <16438795+rooklift@users.noreply.github.com>
2022-12-05 17:07:31 +0000

    Handle errors in log stream

--- src/modules/log.js
+++ src/modules/log.js
@@ -15,6 +15,7 @@ module.exports = function(s) {
 		if (logfilename) {
 			console.log(`Closing ${logfilename}`);
 			stream.end();
+			stream = null;
 			logfilename = null;
 		}
 		return;
@@ -26,11 +27,22 @@ module.exports = function(s) {
 		if (logfilename) {
 			console.log(`Closing log ${logfilename}`);
 			stream.end();
+			stream = null;
 			logfilename = null;
 		}
 		console.log(`Logging to ${config.logfile}`);
+		let new_stream = fs.createWriteStream(config.logfile, {flags: "a"});
+		new_stream.on("error", (err) => {
+			console.log(err);
+			new_stream.end();
+			if (stream === new_stream) {
+				stream = null;
+				logfilename = null;
+				hub.set("logfile", null);
+			}
+		}
+		stream = new_stream;
 		logfilename = config.logfile;
-		stream = fs.createWriteStream(config.logfile, {flags: "a"});
 	}
 
 	if (typeof s !== "string") {
