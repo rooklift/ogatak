ba329ba0b2a09dd3108a8cba0623621eebbb0a94
rooklift <16438795+rooklift@users.noreply.github.com>
2024-01-30 23:00:52 +0000

    Revert "Update decoders.js"
    This reverts commit 76976803499394227e588e2acb226175bb91a265.

--- src/modules/decoders.js
+++ src/modules/decoders.js
@@ -7,26 +7,32 @@ const util = require("util");
 
 let decoder_cache = Object.create(null);
 
-exports.get_decoder = function(encoding) {
+module.exports = {
 
-	// This can throw if encoding is not supported.
+	get_decoder: function(encoding) {
 
-	if (decoder_cache[encoding] === undefined) {
-		decoder_cache[encoding] = new util.TextDecoder(encoding);
-	}
+		// This can throw if encoding is not supported.
 
-	return decoder_cache[encoding];
+		if (decoder_cache[encoding] === undefined) {
+			decoder_cache[encoding] = new util.TextDecoder(encoding);
+		}
+
+		return decoder_cache[encoding];
+
+	},
+
+	available: function(encoding) {
+
+		// Tests whether it's possible for us to decode the given encoder.
+		// If it is, the relevant decoder will be created if it doesn't exist.
+
+		try {
+			this.get_decoder(encoding);
+			return true;
+		} catch (err) {
+			return false;
+		}
+	},
+
 };
 
-exports.available = function(encoding) {
-
-	// Tests whether it's possible for us to decode the given encoder.
-	// If it is, the relevant decoder will be created if it doesn't exist.
-
-	try {
-		exports.get_decoder(encoding);
-		return true;
-	} catch (err) {
-		return false;
-	}
-};
