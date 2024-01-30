76976803499394227e588e2acb226175bb91a265
rooklift <16438795+rooklift@users.noreply.github.com>
2024-01-30 22:44:38 +0000

    Update decoders.js

--- src/modules/decoders.js
+++ src/modules/decoders.js
@@ -7,32 +7,26 @@ const util = require("util");
 
 let decoder_cache = Object.create(null);
 
-module.exports = {
+exports.get_decoder = function(encoding) {
 
-	get_decoder: function(encoding) {
+	// This can throw if encoding is not supported.
 
-		// This can throw if encoding is not supported.
+	if (decoder_cache[encoding] === undefined) {
+		decoder_cache[encoding] = new util.TextDecoder(encoding);
+	}
 
-		if (decoder_cache[encoding] === undefined) {
-			decoder_cache[encoding] = new util.TextDecoder(encoding);
-		}
-
-		return decoder_cache[encoding];
-
-	},
-
-	available: function(encoding) {
-
-		// Tests whether it's possible for us to decode the given encoder.
-		// If it is, the relevant decoder will be created if it doesn't exist.
-
-		try {
-			this.get_decoder(encoding);
-			return true;
-		} catch (err) {
-			return false;
-		}
-	},
-
+	return decoder_cache[encoding];
 };
 
+exports.available = function(encoding) {
+
+	// Tests whether it's possible for us to decode the given encoder.
+	// If it is, the relevant decoder will be created if it doesn't exist.
+
+	try {
+		exports.get_decoder(encoding);
+		return true;
+	} catch (err) {
+		return false;
+	}
+};
