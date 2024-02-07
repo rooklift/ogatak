7c6a89a81377b26f296b44210ca258e9749a767a
rooklift <16438795+rooklift@users.noreply.github.com>
2024-02-07 11:37:17 +0000

    skip some tests when msg is string

--- src/modules/stringify.js
+++ src/modules/stringify.js
@@ -4,24 +4,21 @@
 // Helps with sending messages over IPC, displaying alerts, etc.
 
 module.exports = (msg) => {
-
-	try {
-
-		if (msg instanceof Error) {
-			msg = msg.toString();
-		} else if (typeof msg === "object") {
-			msg = JSON.stringify(msg);
-		} else if (typeof msg === "undefined") {
-			msg = "undefined";
-		} else {
-			msg = msg.toString();
+	if (typeof msg !== "string") {
+		try {
+			if (msg instanceof Error) {
+				msg = msg.toString();
+			} else if (typeof msg === "object") {
+				msg = JSON.stringify(msg);
+			} else if (typeof msg === "undefined") {
+				msg = "undefined";
+			} else {
+				msg = msg.toString();
+			}
+		} catch (err) {
+			return "stringify() failed";
 		}
-		msg = msg.trim();
-		return msg;
-
-	} catch (err) {
-
-		return "stringify() failed";
-
 	}
+	msg = msg.trim();
+	return msg;
 };
