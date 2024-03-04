9c3749ff70fff09069a594bf7c3e71f1e63c3b13
rooklift <16438795+rooklift@users.noreply.github.com>
2024-03-04 17:14:42 +0000

    jshint

--- src/modules/hub.js
+++ src/modules/hub.js
@@ -1269,7 +1269,7 @@ let hub_main_props = {
 			this.draw();
 		} else if (["AB", "AW", "AE"].includes(config.editing)) {
 			this.halt();
-			delete config.avoid_set[s]							// Reasonable in all cases.
+			delete config.avoid_set[s];							// Reasonable in all cases.
 			let key = config.editing;
 			if (event.button === 2 && key !== "AE") {
 				// When user right click with Add Black or Add White, use the other colour instead.
