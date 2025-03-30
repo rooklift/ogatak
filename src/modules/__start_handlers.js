5d717ab018da32565cdcec833647b02387083ea2
rooklift <16438795+rooklift@users.noreply.github.com>
2025-03-30 20:57:36 +0100

    Update __start_handlers.js

--- src/modules/__start_handlers.js
+++ src/modules/__start_handlers.js
@@ -22,20 +22,20 @@ const {event_path_class_string} = require("./utils");
 
 const get_path_for_file = (webUtils && webUtils.getPathForFile) ? webUtils.getPathForFile : file => file.path;
 
-// mousedown events in Electron 24-28 (at least) can be completely fake, see Electron issue #38322, I hate it so much...
+// mousedown events since Electron 24 can be completely fake, see Electron issue #38322, I hate it so much...
 // Anyway in those versions we declare mousedown_event_is_electron_bug() to test for whether a click is fake...
 
-let mousedown_event_is_electron_bug = () => false;
+const mousedown_event_is_electron_bug = parseInt(process.versions.electron) < 24 ?
 
-if ([24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35].includes(parseInt(process.versions.electron))) {
-	mousedown_event_is_electron_bug = (event) => {
+	() => false :
+
+	(event) => {
 		if (event.button === 0 && event.buttons === 2) {
 			console.log("Electron bugged click detected.");
 			return true;
 		}
 		return false;
 	};
-}
 
 // Wheel should scroll the current game...
 
